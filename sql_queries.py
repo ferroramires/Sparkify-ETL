a8637c74a35cc3ba8aa01b41eb1bdee81662a95c
Felipe Ferro Ramires <ferro.ramires@gmail.com>
2021-08-25 11:44:40 -0300

    Add files via upload

--- sql_queries.py
+++ sql_queries.py
@@ -27,8 +27,8 @@ user_table_create = ("""CREATE TABLE IF NOT EXISTS user_table (
     user_id VARCHAR PRIMARY KEY,
     first_name VARCHAR,
     last_name VARCHAR,
-    gender BOOLEAN,
-    level INT
+    gender VARCHAR,
+    level VARCHAR
     );
 """)
 
@@ -45,13 +45,13 @@ artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (
     artist_id VARCHAR PRIMARY KEY,
     name VARCHAR,
     location VARCHAR,
-    latitude INT,
-    longitude INT
+    latitude REAL,
+    longitude REAL
     );
 """)
 
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time_table (
-    start_time BIGINT PRIMARY KEY,
+    start_time timestamp PRIMARY KEY,
     hour INT,
     day INT,
     week INT,
@@ -71,6 +71,7 @@ songplay_table_insert = ("""INSERT INTO song_play(songplay_id, start_time, user_
 user_table_insert = ("""INSERT INTO user_table(user_id, first_name, last_name, gender, level)
     VALUES
     (%s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING
 """)
 
 song_table_insert = ("""INSERT INTO songs(song_id, title, artist_id, year, duration)
@@ -80,19 +81,23 @@ song_table_insert = ("""INSERT INTO songs(song_id, title, artist_id, year, durat
 
 artist_table_insert = ("""INSERT INTO artists(artist_id, name, location, latitude, longitude)
     VALUES
-    ON CONFLICT artist_id DO UPDATE
     (%s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING;
 """)
 
 
 time_table_insert = ("""INSERT INTO time_table(start_time, hour, day, week, month, year, weekday)
     VALUES
     (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING;
 """)
 
 # FIND SONGS
 
-song_select = ("""
+song_select = ("""SELECT s.song_id, a.artist_id
+FROM songs AS s
+JOIN artists as a
+ON s.artist_id=a.artist_id;
 """)
 
 # QUERY LISTS
